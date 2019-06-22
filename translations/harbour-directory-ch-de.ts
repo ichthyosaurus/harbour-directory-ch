29fa0557033c7373cdbb179b8633b7a134448844
Mirian Margiani <mirian@margiani.ch>
2019-06-22 21:34:03 +0200

    update translations

--- translations/harbour-directory-ch-de.ts
+++ translations/harbour-directory-ch-de.ts
@@ -1,37 +1,100 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.1" language="de_CH">
 <context>
-    <name>CoverPage</name>
+    <name></name>
+    <message id="components_contacts-ph-vcard_name">
+        <source>contact</source>
+        <extracomment>Placeholder name for contact vcard filename</extracomment>
+        <translation>Kontakt</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
     <message>
-        <source>My Cover</source>
-        <translation>Mein Cover</translation>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation>Daten</translation>
+    </message>
+    <message>
+        <source>Search.ch...</source>
+        <translation>Search.ch ...</translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation>Webseite</translation>
+    </message>
+    <message>
+        <source>https://search.ch/</source>
+        <translation>https://search.ch/</translation>
+    </message>
+</context>
+<context>
+    <name>ContactCardPage</name>
+    <message>
+        <source>Share</source>
+        <translation>Teilen</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Speichern</translation>
     </message>
 </context>
 <context>
     <name>FirstPage</name>
     <message>
-        <source>Show Page 2</source>
-        <translation>Zur Seite 2</translation>
+        <source>About</source>
+        <translation>Über</translation>
     </message>
     <message>
-        <source>UI Template</source>
-        <translation>UI-Vorlage</translation>
+        <source>Directory</source>
+        <translation>Telefonbuch</translation>
     </message>
     <message>
-        <source>Hello Sailors</source>
-        <translation>Hallo Matrosen</translation>
+        <source>What?</source>
+        <translation>Was?</translation>
+    </message>
+    <message>
+        <source>Where?</source>
+        <translation>Wo?</translation>
+    </message>
+    <message>
+        <source>Details • Save • Share</source>
+        <translation>Details • Speichern • Teilen</translation>
+    </message>
+    <message>
+        <source>published: %1</source>
+        <translation>veröff.: %1</translation>
+    </message>
+    <message>
+        <source>updated: %1</source>
+        <translation>aktualisiert: %1</translation>
+    </message>
+    <message>
+        <source>status: %1</source>
+        <translation>Stand: %1</translation>
     </message>
 </context>
 <context>
-    <name>SecondPage</name>
+    <name>harbour-directory-ch</name>
     <message>
-        <source>Nested Page</source>
-        <translation>Unterseite</translation>
+        <source>d M yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
+        <translation>d. M. yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</translation>
     </message>
     <message>
-        <source>Item</source>
-        <translation>Element</translation>
+        <source>hh&apos;:&apos;mm</source>
+        <translation>hh&apos;:&apos;mm</translation>
     </message>
 </context>
 </TS>
--- translations/harbour-directory-ch.ts
+++ translations/harbour-directory-ch.ts
@@ -1,36 +1,99 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.1">
 <context>
-    <name>CoverPage</name>
+    <name></name>
+    <message id="components_contacts-ph-vcard_name">
+        <source>contact</source>
+        <extracomment>Placeholder name for contact vcard filename</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
     <message>
-        <source>My Cover</source>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search.ch...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>https://search.ch/</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ContactCardPage</name>
+    <message>
+        <source>Share</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>FirstPage</name>
     <message>
-        <source>Show Page 2</source>
+        <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>UI Template</source>
+        <source>Directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Hello Sailors</source>
+        <source>What?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Where?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Details • Save • Share</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>published: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>updated: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>status: %1</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>SecondPage</name>
+    <name>harbour-directory-ch</name>
     <message>
-        <source>Nested Page</source>
+        <source>d M yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Item</source>
+        <source>hh&apos;:&apos;mm</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
